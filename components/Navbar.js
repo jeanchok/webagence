7b1fe451ca22eef35600e6e25ff7d7d8e8a5974b
jeanchok <48122647+jeanchok@users.noreply.github.com>
2023-04-07 18:27:41 +0200

    project front page and mobile menu design

--- components/Navbar.js
+++ components/Navbar.js
@@ -21,41 +21,43 @@ const Navbar = () => {
                 <div className="bar3"></div>
             </button>
             <ul className={!menuClass ? 'none' : 'list'}>
+                <div className={!menuClass ? 'none' : 'listInner'}>
+                    <li className={router.pathname == "/" ? "activeLine" : ""}>
 
-                <li className={router.pathname == "/" ? "activeLine" : ""}>
-
-                    <Link href="/">
-                        <a className={router.pathname == "/" ? "active" : ""}>
-                            Accueil
-                        </a></Link>
-                </li>
-                <li className={router.pathname == "/projets" ? "activeLine" : ""}>
-                    <Link href="/projets">
-                        <a className={router.pathname == "/projets" ? "active" : ""}>
-                            Projets
-                        </a></Link>
-                </li>
-                <li className={router.pathname == "/offres" ? "activeLine" : ""}>
-                    <Link href="/offres">
-                        <a className={router.pathname == "/offres" ? "active" : ""}>
-                            Nos offres
-                        </a></Link>
-                </li>
-                <li className={router.pathname == "/agence" ? "activeLine" : ""}>
-                    <Link href="/agence">
-                        <a className={router.pathname == "/agence" ? "active" : ""}>
-                            L&apos;agence
-                        </a>
-                    </Link>
-                </li>
-                <li className={router.pathname == "/contact" ? "activeLine" : ""}>
-                    <Link href="/contact">
-                        <a className={router.pathname == "/contact" ? "active" : ""}>
-                            Contact
-                        </a>
-                    </Link>
-                </li>
+                        <Link href="/">
+                            <a className={router.pathname == "/" ? "active" : ""}>
+                                Accueil
+                            </a></Link>
+                    </li>
+                    <li className={router.pathname == "/projets" ? "activeLine" : ""}>
+                        <Link href="/projets">
+                            <a className={router.pathname == "/projets" ? "active" : ""}>
+                                Projets
+                            </a></Link>
+                    </li>
+                    <li className={router.pathname == "/offres" ? "activeLine" : ""}>
+                        <Link href="/offres">
+                            <a className={router.pathname == "/offres" ? "active" : ""}>
+                                Nos offres
+                            </a></Link>
+                    </li>
+                    <li className={router.pathname == "/agence" ? "activeLine" : ""}>
+                        <Link href="/agence">
+                            <a className={router.pathname == "/agence" ? "active" : ""}>
+                                L&apos;agence
+                            </a>
+                        </Link>
+                    </li>
+                    <li className={router.pathname == "/contact" ? "activeLine" : ""}>
+                        <Link href="/contact">
+                            <a className={router.pathname == "/contact" ? "active" : ""}>
+                                Contact
+                            </a>
+                        </Link>
+                    </li>
+                </div>
             </ul>
+
         </nav>
 
     );
