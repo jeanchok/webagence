e2b7050404bb5875b89fb4c7153c1ff214d39f5b
jeanchok <48122647+jeanchok@users.noreply.github.com>
2023-06-14 15:19:23 +0200

    removing d2m

--- projectsData.js
+++ projectsData.js
@@ -10,17 +10,17 @@ export const projects = [
         mockup: "/img/smartmockups_letwjg88.jpg",
         link: "https://agathafestival.fr/",
     },
-    {
-        id: 2,
-        title: "D2M",
-        date: "Janvier 2022",
-        languages: ["Wordpress"],
-        infos:
-            "Réalisation de l'intégralité d'un site vitrine pour un revendeur de machine à bois, Laillé (35).",
-        img: "/img/d2m_img.jpg",
-        mockup: "/img/d2m_mockup.png",
-        link: "https://d2m-machine-bois.fr/",
-    },
+    // {
+    //     id: 2,
+    //     title: "D2M",
+    //     date: "Janvier 2022",
+    //     languages: ["Wordpress"],
+    //     infos:
+    //         "Réalisation de l'intégralité d'un site vitrine pour un revendeur de machine à bois, Laillé (35).",
+    //     img: "/img/d2m_img.jpg",
+    //     mockup: "/img/d2m_mockup.png",
+    //     link: "https://d2m-machine-bois.fr/",
+    // },
     {
         id: 3,
         title: "Bulle de rêve",
